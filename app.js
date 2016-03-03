467476ce38c96721dc51fa8736aadb2fcbe285ce
Joe Kent <thedeadlybutter@gmail.com>
2016-03-02 22:38:58 -0500

    Adds first two plans and base styling and markup

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  partialsDir: [__dirname + '/views/partials'],
+  helpers: {
+  }
+}));
+app.set('view engine', 'handlebars');
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var data = require('./economic_data');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home', data);
 });
 
 app.listen(process.env.PORT, function () {
